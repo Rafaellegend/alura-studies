a0d14cadfd8f1c49c3a5701efdead59e8a62ac85
Raffozo <rafaellegendofdark@hotmail.com>
2022-12-13 20:24:53 -0300

    First Commit

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { JsxElement } from "typescript";
+import style from './Button.module.scss';
+
+class Button extends React.Component<{children?: React.ReactNode, type?: "button" | "submit" | "reset" | undefined}> {
+    render(): React.ReactNode {
+        const { type = "button" } = this.props;
+        return ( 
+            <button type={type} className={style.botao}>
+                {this.props.children}
+            </button>
+        )
+    }
+}
+
+export default Button;
---
+++ src/components/List/index.tsx
@@ -0,0 +1,20 @@
+import style from './List.module.scss'
+import Item from './Item'
+import { ITarefa } from '../../types/task';
+
+
+function List({tarefas}:{tarefas: ITarefa[]}){
+ 
+    return (
+        <aside className={style.listaTarefas}>
+            <h2> Estudos do dia</h2>
+            <ul>
+                {tarefas.map((item, index) =>(
+                  <Item key={index} {...item}/>
+                ))}
+            </ul>
+        </aside>
+    )
+}
+
+export default List;
